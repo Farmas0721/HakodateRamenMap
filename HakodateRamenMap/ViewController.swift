e21ac3b610dd5311e62d35dcd727c9d7929d8106
中村　朝陽 <wsk.nakamura@gmail.com>
2019-02-19 03:58:42 +0900

    Initial Commit

---
+++ HakodateRamenMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HakodateRamenMap
+//
+//  Created by 中村　朝陽 on 2019/02/19.
+//  Copyright © 2019年 asahi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
