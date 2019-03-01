17580f262e3599422d76901de8acd7d54e6d28c3
山田楓也 <fuya0713@gmail.com>
2019-03-02 02:09:06 +0900

    土台作ったよ

---
+++ HakodateRamenMap/map/Map.swift
@@ -0,0 +1,30 @@
+//
+//  Map.swift
+//  HakodateRamenMap
+//
+//  Created by Fuuya Yamada on 2019/03/02.
+//  Copyright © 2019 asahi. All rights reserved.
+//
+
+import UIKit
+
+class Map: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HakodateRamenMap/mypage/MyPage.swift
@@ -0,0 +1,30 @@
+//
+//  MyPage.swift
+//  HakodateRamenMap
+//
+//  Created by Fuuya Yamada on 2019/03/02.
+//  Copyright © 2019 asahi. All rights reserved.
+//
+
+import UIKit
+
+class MyPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
