39b8c849c0381f16f28d0aab26997e0e236135ff
Fuyan777 <fuyamiwa713@gmail.com>
2019-04-08 11:01:55 +0900

    カメラロール取得

---
+++ HakodateRamenMap/timeline/SelectedPhoto.swift
@@ -0,0 +1,30 @@
+//
+//  SelectedPhoto.swift
+//  HakodateRamenMap
+//
+//  Created by 山田楓也 on 2019/04/07.
+//  Copyright © 2019 asahi. All rights reserved.
+//
+
+import UIKit
+
+class SelectedPhoto: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
