40edcb174eeeeb91aa94d17df554efc9f3df8798
Fuyan777 <fuyamiwa713@gmail.com>
2019-05-04 22:40:02 +0900

    cellの詳細ファイルを作成した

---
+++ HakodateRamenMap/timeline/Detail.swift
@@ -0,0 +1,30 @@
+//
+//  Detail.swift
+//  HakodateRamenMap
+//
+//  Created by 山田楓也 on 2019/05/04.
+//  Copyright © 2019 asahi. All rights reserved.
+//
+
+import UIKit
+
+class Detail: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
