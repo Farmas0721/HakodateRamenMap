04a15b8599866f7bb0635e79177db62fb93b3248
Fuyan777 <fuyamiwa713@gmail.com>
2019-05-05 19:51:39 +0900

    詳細画面を作成(遷移とデータ取得のみ作成)

--- HakodateRamenMap/timeline/Detail.swift
+++ HakodateRamenMap/timeline/Detail.swift
@@ -7,13 +7,78 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseUI
 
-class Detail: UIViewController {
+class Detail: UIViewController ,UITableViewDelegate, UITableViewDataSource{
+  
+    @IBOutlet weak var detailTable: UITableView!
+    
+    var contentArray: [DataSnapshot] = []
+    
+    var snap: DataSnapshot!
+    var selectedSnap: DataSnapshot!
+    
+    let ref = Database.database().reference()
+    let storage = Storage.storage()
+    var store = ""
+    
+    var detailSnap: DataSnapshot! //ListTableからのデータの受け取りのための変数
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor =  UIColor.rgba(red: 242, green: 92, blue: 0, alpha: 1)
         // Do any additional setup after loading the view.
+        detailTable.delegate = self as UITableViewDelegate
+        detailTable.dataSource = self as UITableViewDataSource
+        detailTable.register(UITableViewCell.self, forCellReuseIdentifier: "nameCell")
+        detailTable.register(UITableViewCell.self, forCellReuseIdentifier: "imageCell")
+        detailTable.register(UITableViewCell.self, forCellReuseIdentifier: "storeNameCell")
+        detailTable.register(UITableViewCell.self, forCellReuseIdentifier: "tasteCell")
+        detailTable.register(UITableViewCell.self, forCellReuseIdentifier: "valueCell")
+        detailTable.reloadData()
+    }
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let namecell = tableView.dequeueReusableCell(withIdentifier: "nameCell", for: indexPath)
+        let imagecell = tableView.dequeueReusableCell(withIdentifier: "imageCell", for: indexPath)
+        let storecell = tableView.dequeueReusableCell(withIdentifier: "storeNameCell", for: indexPath)
+        let tastecell = tableView.dequeueReusableCell(withIdentifier: "tasteCell", for: indexPath)
+        let valuecell = tableView.dequeueReusableCell(withIdentifier: "valueCell", for: indexPath)
+        
+        
+        //受け取ったselectedSnapを辞書型に変換
+        let item = detailSnap.value as! Dictionary<String, AnyObject>
+          switch indexPath.row{
+          case 0:
+            namecell.textLabel?.text = item["storeName"] as? String
+            //cell.imageView?.image =
+            return namecell
+          case 1:
+            let urlstring = String(describing: item["imageID"]!)
+            let urlimage = URL(string: urlstring)
+            imagecell.imageView!.sd_setImage(with: urlimage)
+            return imagecell
+          case 2:
+            namecell.textLabel?.text = item["storeName"] as? String
+            return storecell
+          case 3:
+            tastecell.textLabel?.text = item["taste"] as? String
+            return tastecell
+          case 4:
+            valuecell.textLabel?.text = item["ramenValue"] as? String
+            return valuecell
+          default:
+            namecell.textLabel?.text = item["storeName"] as? String
+            return namecell
+        }
     }
     
 
