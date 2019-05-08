b16d9e5bedd59ea62d3218c0bc4fef1d65404933
たかなが <b1017033@gmail.com>
2019-05-08 20:28:28 +0900

    パスワード再認証

---
+++ HakodateRamenMap/login/ReregisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ReregisterViewController.swift
+//  HakodateRamenMap
+//
+//  Created by 永野誉也 on 2019/04/25.
+//  Copyright © 2019 asahi. All rights reserved.
+//
+
+import UIKit
+
+class ReregisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
